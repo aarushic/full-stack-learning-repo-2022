6ac55aae92ed7a422327f5f9fecd15c60c06ff1a
aarushi chitagi <aarushichitagi@wireless-10-146-5-99.public.utexas.edu>
2022-10-28 15:43:12 -0500

    week3

---
+++ Week_4/homework/weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
+}
